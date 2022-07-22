b68e54d84acff834a8a20c7fb1afc67b32883b65
Uttam  Garambha <86154633+Uttamgarambha@users.noreply.github.com>
2022-07-22 07:55:01 +0530

    Add files via upload

---
+++ guessGame.py
@@ -0,0 +1,20 @@
+import random
+n = random.randint(0,100)
+number_of_guesses = 1
+print("Nuumber of guesses is limited to only 9 times: ")
+while(number_of_guesses<=9):
+    guess_number = int(input("Guess the number :\n"))
+    if guess_number <n:
+        print("You enter less number please input greater number\n")
+
+    elif guess_number >n:
+            print("you enter greater number please inout smaller number\n")
+    else:
+        print("you won\n")
+        print(number_of_guesses,"no.of guesses he took to finish")
+        break
+    print(9-number_of_guesses,"no. of guesses left")
+    number_of_guesses = number_of_guesses+1
+
+    if(number_of_guesses>9):
+        print("Game over")
